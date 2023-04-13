acf9bf678ef7a1da6ed7e46cd79eb09cfc4404e4
Kevin <58339856+kw2828@users.noreply.github.com>
2023-04-13 17:13:17 -0400

    Update colab code to be used with the free version

--- dolly_2.0_colab.py
+++ dolly_2.0_colab.py
@@ -1,14 +1,15 @@
-!pip -q install datasets loralib sentencepiece
 !pip -q install git+https://github.com/huggingface/transformers # need to install from github
-!pip -q install git+https://github.com/huggingface/peft.git
-!pip -q install bitsandbytes
-!pip install torch
+!pip -q install accelerate>=0.12.0 
 
 import torch
 from transformers import pipeline
 
-instruct_pipeline = pipeline(model="databricks/dolly-v2-12b", torch_dtype=torch.bfloat16, trust_remote_code=True, device_map="auto")
+# use dolly-v2-12b if you're using Colab Pro+, using pythia-2.8b for Free Colab
+generate_text = pipeline(model="databricks/dolly-v2-2-8b", 
+                         torch_dtype=torch.bfloat16, 
+                         trust_remote_code=True,
+                         device_map="auto")
 
-instruct_pipeline("Explain to me the difference between nuclear fission and fusion.")
+generate_text("Explain to me the difference between nuclear fission and fusion.")
 
-instruct_pipeline("Explain self-attention and transformer to me like I'm 5 years old (ELI5).")
+generate_text("Explain self-attention and transformer to me like I'm 5 years old (ELI5).")
