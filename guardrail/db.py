afb64f424cf99cbf6c475a8371faa07849065155
Kevin <58339856+kw2828@users.noreply.github.com>
2023-07-13 14:15:20 -0400

    Feature/v0.0.01 (#12)
    * Initial commit for v0.0.01
    
    * Update README.md
    
    * Delete guardrailmlenv directory
    
    * Update Toxicity Model and add Bias Model
    
    * Update README.md
    
    * Update logging and model imports

--- guardrail/db.py
+++ guardrail/db.py
@@ -1,12 +1,12 @@
 import sqlite3
 
-def insert_log(metric, message, model_id):
+def insert_log(model_uri, prompt, output, metric_name, metric_value):
     # Create a database connection
     conn = sqlite3.connect("logs.db")
 
     # Insert the log into the database
-    conn.execute("INSERT INTO logs (timestamp, levelname, message, model_id) VALUES (datetime('now'), ?, ?, ?)",
-                 (metric, message, model_id))
+    conn.execute("INSERT INTO logs (timestamp, model_uri, prompt, output, metric_name, metric_value) VALUES (datetime('now'), ?, ?, ?, ?, ?)",
+                 (model_uri, prompt, output, metric_name, metric_value))
 
     # Commit the changes and close the connection
     conn.commit()
